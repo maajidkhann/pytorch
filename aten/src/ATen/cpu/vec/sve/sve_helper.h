b6d2b50cf9b7f91182d423e606c87c5ed2a12b88
maajidkhann <maajidkhan.n@fujitsu.com>
2024-11-14 16:36:27 +0530

    FRIPL FINAL IMPL
    Signed-off-by: maajidkhann <maajidkhan.n@fujitsu.com>

--- aten/src/ATen/cpu/vec/sve/sve_helper.h
+++ aten/src/ATen/cpu/vec/sve/sve_helper.h
@@ -60,42 +60,35 @@ typedef svfloat64_t vls_float64_t __attribute__((arm_sve_vector_bits(VECTOR_WIDT
 #define ALL_F64_TRUE_MASK svreinterpret_f64_s64(ALL_S64_TRUE_MASK)
 #define ALL_F64_FALSE_MASK svreinterpret_f64_s64(ALL_S64_FALSE_MASK)
 
-  inline svfloat32_t svexp_f32_x(svbool_t pg, svfloat32_t src) {
+inline svfloat32_t svexp_f32_x(svbool_t pg, svfloat32_t src) {
     // Constants
-        const auto log2_e = svdup_n_f32(1.0f / std::log(2.0f));
-        //const auto ln2 = svdup_n_f32(0.6931473921f);
-        //const auto half_ln2_sq = svdup_n_f32(0.2413862043f);
+    const auto log2_e = svdup_n_f32(1.4426950409f);
+    const auto ln2 = svdup_n_f32(0.6931473921f);
+    const auto half_ln2_sq = svdup_n_f32(0.2413862043f);
 
-        const auto ln2 = svdup_n_f32(0.6931471805599453f);
-        const auto half_ln2_sq = svdup_n_f32(0.2413862295f); // More precise value
+    const auto not_mask17 = svdup_n_u32(~((1u << 17) - 1));
+    const auto one = svdup_n_f32(1.0f);
 
-        const auto not_mask17 = svdup_n_u32(~((1u << 17) - 1));
-        const auto one = svdup_n_f32(1.0f);
-        const auto zero = svdup_n_f32(0.0f);
-        const auto inf = svdup_n_f32(std::numeric_limits<float>::infinity());
-        const auto max_input = svdup_n_f32(88.37f);                             // Approximately ln(2^127.5)
-        const auto min_input = svdup_n_f32(-86.64f);                            // Approximately ln(2^-125)
+    // Algorithm starts here
+    svfloat32_t t0 = svmul_f32_z(pg, src, log2_e);                          // y = x * log2(e)
+    svfloat32_t t1 = svrintm_f32_z(pg, t0);                                 // rount to int (float)
+    svint32_t t2 = svcvt_s32_f32_z(pg, t1);                                 // n
 
-        // Algorithm starts here
-        svfloat32_t t0 = svmul_f32_z(pg, src, log2_e);                          // y = x * log2(e)
-        svfloat32_t t1 = svrintm_f32_z(pg, t0);                                 // rount to int (float)
-        svint32_t t2 = svcvt_s32_f32_z(pg, t1);                                 // n
+    t1 = svsub_f32_z(pg, t0, t1);                                           // a = y - floor(y)
+    t1 = svadd_f32_z(pg, t1, one);                                          // b = a + 1
 
-        t1 = svsub_f32_z(pg, t0, t1);                                           // a = y - floor(y)
-        t1 = svadd_f32_z(pg, t1, one);                                          // b = a + 1
+    svuint32_t t3 = svlsr_n_u32_z(pg, svreinterpret_u32_f32(t1), 17);       // v = b >> 17 (u32)
+    svfloat32_t t4 = svexpa_f32(t3);                                        // c = fexpa(v)
+    t4 = svscale_f32_z(pg, t4, t2);                                         // fexpa(v) * 2^(n)
 
-        svuint32_t t3 = svlsr_n_u32_z(pg, svreinterpret_u32_f32(t1), 17);       // v = b >> 17 (u32)
-        svfloat32_t t4 = svexpa_f32(t3);                                        // c = fexpa(v)
-        t4 = svscale_f32_z(pg, t4, t2);                                         // fexpa(v) * 2^(n)
+    // and_(t2.d, t1.d, not_mask17.d)
+    svfloat32_t t5 = svreinterpret_f32_u32(svand_u32_z(pg, svreinterpret_u32_f32(t1), not_mask17));
+    t5 = svsub_f32_z(pg, t1, t5);                                           // z
+    t0 = svmla_f32_z(pg, ln2, t5, half_ln2_sq);                             // ln2 + half_ln2_sq * z
+    t0 = svmla_f32_z(pg, one, t5, t0);                                      // 1 + (ln2 * z) + (half_ln2_sq * z * z)
+    t0 = svmul_f32_z(pg, t0, t4);                                           // Final result
 
-        // and_(t2.d, t1.d, not_mask17.d)
-        svfloat32_t t5 = svreinterpret_f32_u32(svand_u32_z(pg, svreinterpret_u32_f32(t1), not_mask17));
-        t5 = svsub_f32_z(pg, t1, t5);                                           // z
-        t0 = svmla_f32_z(pg, ln2, t5, half_ln2_sq);                             // ln2 + half_ln2_sq * z
-        t0 = svmla_f32_z(pg, one, t5, t0);                                      // 1 + (ln2 * z) + (half_ln2_sq * z * z)
-        t0 = svmul_f32_z(pg, t0, t4);                                           // Final result
-
-        return t0;
-    }
+    return t0;
+}
 
 #endif // defined(CPU_CAPABILITY_SVE)
